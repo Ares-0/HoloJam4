6954c1626a42aafde25da6bda9fc2ebc3607904a
Mason Hoppe <mason.hoppe@gmail.com>
2024-07-21 18:48:18 -0400

    Simplify DialogBox tree to one copy of stuff

--- source/gui/DialogBox.gd
+++ source/gui/DialogBox.gd
@@ -7,24 +7,20 @@ var scene_text = {}
 var in_progress = false
 var frame_updated = 0
 var text_queue = []
+var box_size_normal = true
 
 @onready var background = $Background
 @onready var text_label = $Background/Control/TextLabel
-@onready var background_big = $BackgroundBig
-@onready var text_label_big = $BackgroundBig/Control/TextLabel
-
-var active_bg
-var active_label
 
 func _ready():
 	background.visible = false
-	background_big.visible = false
 	scene_text = load_scene_text()
+
 	DialogBus.connect("display_dialog", on_display_dialog)
 	DialogBus.connect("display_text", on_display_text)
 	DialogBus.connect("display_dialog_big", on_display_dialog_big)
 	DialogBus.connect("display_text_big", on_display_text_big)
-	self.visible = true
+	self.visible = true # ?
 
 func _process(_delta):
 	# if pausing isn't fully ready, dont accept inputs
@@ -51,7 +47,7 @@ func load_scene_text():
 	return JSON.parse_string(file.get_as_text())
 
 func show_text():
-	active_label.text = text_queue.pop_front()
+	text_label.text = text_queue.pop_front()
 
 func next_line():
 	if text_queue.size() > 0:
@@ -61,7 +57,7 @@ func next_line():
 
 func finish():
 	text_label.text = ""
-	active_bg.visible = false
+	background.visible = false
 	in_progress = false
 	get_tree().paused = false # turns out this is very important!
 	DialogBus.dialog_done.emit()
@@ -70,34 +66,52 @@ func display():
 	frame_updated = Engine.get_frames_drawn()
 	if not in_progress:
 		get_tree().paused = true
-		active_bg.visible = true
+		background.visible = true
 		in_progress = true
 		show_text()
 		DialogBus.dialog_start.emit()
+
+func toggle_box_size():
+	if box_size_normal:
+		set_box_big()
+	else:
+		set_box_normal()
+
+func set_box_big():
+	box_size_normal = false
+	background.offset_left = -458.0
+	background.offset_top = -332.0
+	background.offset_right = -90.0
+	background.offset_bottom = -228.0
+	background.scale = Vector2(2.5, 2.5)
+
+func set_box_normal():
+	box_size_normal = true
+	background.offset_left = -269.0
+	background.offset_top = -180.0
+	background.offset_right = 99.0
+	background.offset_bottom = -76.0
+	background.scale = Vector2(1.5, 1.5)
 
 func on_display_dialog(text_key: String):
 	# TODO: better error reporting about failures
 	assert(scene_text.has(text_key))
 
 	text_queue.append_array((scene_text.get(text_key)))
-	active_bg = background
-	active_label = text_label
+	set_box_normal()
 	display()
 
 func on_display_text(raw_text: String):
 	text_queue.append(raw_text)
-	active_bg = background
-	active_label = text_label
+	set_box_normal()
 	display()
 
 func on_display_dialog_big(text_key: String):
 	text_queue.append_array((scene_text.get(text_key)))
-	active_bg = background_big
-	active_label = text_label_big
+	set_box_big()
 	display()
 
 func on_display_text_big(raw_text: String):
 	text_queue.append(raw_text)
-	active_bg = background_big
-	active_label = text_label_big
+	set_box_big()
 	display()
